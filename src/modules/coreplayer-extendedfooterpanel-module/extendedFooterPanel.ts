5f5f9de055dde0298734f422290a94618de465cb
unknown <e.silverton@gmail.com>
2013-10-16 18:18:31 +0100

    add save and download buttons to extended footer

--- src/modules/coreplayer-extendedfooterpanel-module/extendedFooterPanel.ts
+++ src/modules/coreplayer-extendedfooterpanel-module/extendedFooterPanel.ts
@@ -8,6 +8,14 @@ import embed = require("../coreplayer-dialogues-module/embedDialogue");
 
 export class ExtendedFooterPanel extends baseFooter.FooterPanel {
 
+    $downloadButton: JQuery;
+    $saveButton: JQuery;
+    $embedBtn: JQuery;
+    
+    static DOWNLOAD: string = 'footer.onDownload';
+    static SAVE: string = 'footer.onSave';
+    static EMBED: string = 'footer.onEmbed';
+
     constructor($element: JQuery) {
         super($element);
     }
@@ -15,14 +23,36 @@ export class ExtendedFooterPanel extends baseFooter.FooterPanel {
     create(): void {
         super.create();
 
+        this.$downloadButton = $('<a class="imageButton download"></a>');
+        this.$options.append(this.$downloadButton);
+
+        this.$saveButton = $('<a class="imageButton save"></a>');
+        this.$options.append(this.$saveButton);
+
         this.$embedBtn = $('<a href="#" class="imageButton embed"></a>');
-        this.$options.prepend(this.$embedBtn);
+        this.$options.append(this.$embedBtn);        
 
         this.$embedBtn.on('click', (e) => {
             e.preventDefault();
 
-            $.publish(embed.EmbedDialogue.SHOW_EMBED_DIALOGUE);
+            $.publish(ExtendedFooterPanel.EMBED);
         });
+
+        this.$downloadButton.on('click', (e) => {
+            e.preventDefault();
+
+            $.publish(ExtendedFooterPanel.DOWNLOAD);
+        });
+
+        this.$saveButton.on('click', (e) => {
+            e.preventDefault();
+
+            $.publish(ExtendedFooterPanel.SAVE);
+        });
+
+        // only embed button is always visible. hide others by default.
+        this.$downloadButton.hide();
+        this.$saveButton.hide();
     }
 
     resize(): void {
