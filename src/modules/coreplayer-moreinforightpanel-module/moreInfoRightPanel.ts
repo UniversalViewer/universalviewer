19682870361826bc45e65a896607c181bfba4792
unknown <e.silverton@gmail.com>
2013-10-25 15:24:32 +0100

    removed data var

--- src/modules/coreplayer-moreinforightpanel-module/moreInfoRightPanel.ts
+++ src/modules/coreplayer-moreinforightpanel-module/moreInfoRightPanel.ts
@@ -5,7 +5,6 @@ import baseRight = require("../coreplayer-shared-module/rightPanel");
 export class MoreInfoRightPanel extends baseRight.RightPanel {
 
     moreInfoItemTemplate: JQuery;
-    data: any;
     $conditionsLink: JQuery;
 
     constructor($element: JQuery) {
@@ -23,15 +22,19 @@ export class MoreInfoRightPanel extends baseRight.RightPanel {
         super.toggleComplete();
 
         if (this.isUnopened) {
-            this.displayInfo();
+            this.getInfo();
         }
     }
 
-    displayInfo(): void {
+    getInfo(): void {
 
         // show loading icon.
         this.$main.addClass('loading');
 
+        this.displayInfo();
+    }
+
+    displayInfo(): void {
         this.$main.append(this.content.holdingText);
     }
 
