d71e51dcf5d7ba53120211d71ed3788a3815d4ac
edsilv <e.silverton@gmail.com>
2013-12-04 11:38:36 +0000

    don't show tree if archive

--- src/modules/coreplayer-treeviewleftpanel-module/treeViewLeftPanel.ts
+++ src/modules/coreplayer-treeviewleftpanel-module/treeViewLeftPanel.ts
@@ -68,9 +68,22 @@ export class TreeViewLeftPanel extends baseLeft.LeftPanel {
     toggleComplete(): void {
         super.toggleComplete();
 
-        // if this is the first opening, create tree and thumbs views.
+        // if this is the first opening, if it's an archive, show
+        // thumbs, otherwise show tree.
         if (this.isUnopened) {
-            this.openTreeView();
+            var type = this.provider.type;
+
+            if (type == 'archive' ||
+                type == 'boundmanuscript' ||
+                type == 'video' ||
+                type == 'audio' ||
+                type == 'artwork') {
+
+                this.$tabs.hide();
+                this.openThumbsView();
+            } else {
+                this.openTreeView();
+            }
         }
     }
 
