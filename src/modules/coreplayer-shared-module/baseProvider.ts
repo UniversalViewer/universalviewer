8410ae9d908a49e3d28d7604bbad8394295c3cdd
Edward Silverton <e.silverton@gmail.com>
2014-09-09 10:55:47 +0100

    removed return null

--- src/modules/coreplayer-shared-module/baseProvider.ts
+++ src/modules/coreplayer-shared-module/baseProvider.ts
@@ -263,7 +263,6 @@ export class BaseProvider implements IProvider{
     }
 
     getStructureByCanvasIndex(index: number): any {
-        if (!index) return null;
         var canvas = this.getCanvasByIndex(index);
         return this.getCanvasStructure(canvas);
     }
