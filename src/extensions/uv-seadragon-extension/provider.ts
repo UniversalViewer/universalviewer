3da40ea5e615128156595915a570c847cec7f6b1
edsilv <e.silverton@gmail.com>
2015-02-22 10:42:15 +0000

    added locale to embed code

--- src/extensions/uv-seadragon-extension/provider.ts
+++ src/extensions/uv-seadragon-extension/provider.ts
@@ -33,7 +33,7 @@ export class Provider extends baseProvider.BaseProvider implements ISeadragonPro
 
         var configUri = this.config.uri || '';
 
-        var script = String.prototype.format(template, this.manifestUri, this.sequenceIndex, assetIndex, zoom, rotation, configUri, width, height, esu);
+        var script = String.prototype.format(template, this.locale, configUri, this.manifestUri, this.sequenceIndex, assetIndex, zoom, rotation, width, height, esu);
 
         return script;
     }
