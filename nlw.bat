41ef7d9a66ccca4b0dd093f116a4809df8170db7
edsilv <e.silverton@gmail.com>
2015-04-24 14:58:51 +0100

    checkout branch first

--- nlw.bat
+++ nlw.bat
@@ -1,11 +1,11 @@
 call git checkout nlw
 call git submodule update --init
 cd tests
-call git pull origin master
 call git checkout master
+call git pull origin master
 cd ../examples
-call git pull origin nlw
 call git checkout nlw
+call git pull origin nlw
 cd ../src/themes/nlw-theme
-call git pull origin master
 call git checkout master
+call git pull origin master
