669f0e23a71cdc0ffdcf6e5d038d7aa9e38bff79
Sebastian Öberg <sebastian.oberg@gmail.com>
2015-11-25 15:36:18 +0100

    Added option to save user settings to localStorage.

--- src/modules/uv-seadragoncenterpanel-module/SeadragonCenterPanel.ts
+++ src/modules/uv-seadragoncenterpanel-module/SeadragonCenterPanel.ts
@@ -551,7 +551,7 @@ class SeadragonCenterPanel extends CenterPanel {
     }
     
     setNavigatorVisible() {
-        var navigatorEnabled = Utils.Bools.GetBool(this.provider.config.options.navigatorEnabled, true);
+        var navigatorEnabled = Utils.Bools.GetBool(this.provider.getSettings().navigatorEnabled, true);
 
         this.viewer.navigator.setVisible(navigatorEnabled);
         
