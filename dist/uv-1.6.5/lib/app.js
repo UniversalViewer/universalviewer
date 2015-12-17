71c09b7772999564b0b16625ab7c4684e14480b3
edsilv <e.silverton@gmail.com>
2015-12-17 16:40:21 +0000

    npmignore change

--- dist/uv-1.6.5/lib/app.js
+++ dist/uv-1.6.5/lib/app.js
@@ -2971,7 +2971,7 @@ define('modules/uv-moreinforightpanel-module/MoreInfoRightPanel',["require", "ex
 });
 
 define('_Version',["require", "exports"], function (require, exports) {
-    exports.Version = '1.6.4';
+    exports.Version = '1.6.5';
 });
 
 var __extends = (this && this.__extends) || function (d, b) {
--- src/_Version.ts
+++ src/_Version.ts
@@ -1,1 +1,1 @@
-export var Version = '1.6.4';
+export var Version = '1.6.5';
