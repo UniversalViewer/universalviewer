9a70abccca5ca893443ff58b4dfa6c31677d6e4a
edsilv <e.silverton@gmail.com>
2016-05-26 17:44:00 +0100

    updated manifesto

--- src/lib/manifesto.js
+++ src/lib/manifesto.js
@@ -849,11 +849,13 @@ var Manifesto;
                     if (!width) {
                         width = resource.getWidth();
                     }
-                    var service = services[0];
-                    id = service.id;
-                    quality = Manifesto.Utils.getImageQuality(service.getProfile());
+                    if (services.length) {
+                        var service = services[0];
+                        id = service.id;
+                        quality = Manifesto.Utils.getImageQuality(service.getProfile());
+                    }
                 }
-                else {
+                if (!id) {
                     return "undefined" == typeof this.__jsonld.thumbnail
                         ? null : this.__jsonld.thumbnail;
                 }
