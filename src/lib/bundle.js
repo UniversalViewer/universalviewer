4cba5a506f2d20c919e713dc7989e9574b323588
edsilv <e.silverton@gmail.com>
2016-07-05 09:51:41 +0100

    remove lodash from ExternalResource

--- src/lib/bundle.js
+++ src/lib/bundle.js
@@ -13390,6 +13390,113 @@ var Manifold;
 
 var Manifold;
 (function (Manifold) {
+    var ExternalResource = (function () {
+        function ExternalResource(resource, dataUriFunc) {
+            this.isResponseHandled = false;
+            resource.externalResource = this;
+            this.dataUri = dataUriFunc(resource);
+            this._parseAuthServices(resource);
+            //this.profile = (<Manifesto.IService>resource).getProfile();
+        }
+        ExternalResource.prototype._parseAuthServices = function (resource) {
+            this.clickThroughService = manifesto.getService(resource, manifesto.ServiceProfile.clickThrough().toString());
+            this.loginService = manifesto.getService(resource, manifesto.ServiceProfile.login().toString());
+            this.restrictedService = manifesto.getService(resource, manifesto.ServiceProfile.restricted().toString());
+            // todo: create this.preferredService?
+            if (this.clickThroughService) {
+                this.logoutService = this.clickThroughService.getService(manifesto.ServiceProfile.logout().toString());
+                this.tokenService = this.clickThroughService.getService(manifesto.ServiceProfile.token().toString());
+            }
+            else if (this.loginService) {
+                this.logoutService = this.loginService.getService(manifesto.ServiceProfile.logout().toString());
+                this.tokenService = this.loginService.getService(manifesto.ServiceProfile.token().toString());
+            }
+            else if (this.restrictedService) {
+                this.logoutService = this.restrictedService.getService(manifesto.ServiceProfile.logout().toString());
+                this.tokenService = this.restrictedService.getService(manifesto.ServiceProfile.token().toString());
+            }
+        };
+        ExternalResource.prototype.isAccessControlled = function () {
+            if (this.clickThroughService || this.loginService || this.restrictedService) {
+                return true;
+            }
+            return false;
+        };
+        ExternalResource.prototype.getData = function (accessToken) {
+            var that = this;
+            return new Promise(function (resolve, reject) {
+                // check if dataUri ends with info.json
+                // if not issue a HEAD request.
+                var type = 'GET';
+                // todo: use manifesto.hasServiceDescriptor
+                if (!that.dataUri.endsWith('info.json')) {
+                    // If access control is unnecessary, short circuit the process.
+                    // Note that isAccessControlled check for short-circuiting only
+                    // works in the "binary resource" context, since in that case,
+                    // we know about access control from the manifest. For image
+                    // resources, we need to check info.json for details and can't
+                    // short-circuit like this.
+                    if (!that.isAccessControlled()) {
+                        that.status = HTTPStatusCode.OK;
+                        resolve(that);
+                        return;
+                    }
+                    type = 'HEAD';
+                }
+                $.ajax({
+                    url: that.dataUri,
+                    type: type,
+                    dataType: 'json',
+                    beforeSend: function (xhr) {
+                        if (accessToken) {
+                            xhr.setRequestHeader("Authorization", "Bearer " + accessToken.accessToken);
+                        }
+                    }
+                }).done(function (data) {
+                    // if it's a resource without an info.json
+                    // todo: if resource doesn't have a @profile
+                    if (!data) {
+                        that.status = HTTPStatusCode.OK;
+                        resolve(that);
+                    }
+                    else {
+                        var uri = unescape(data['@id']);
+                        that.data = data;
+                        that._parseAuthServices(that.data);
+                        // remove trailing /info.json
+                        if (uri.endsWith('/info.json')) {
+                            uri = uri.substr(0, uri.lastIndexOf('/'));
+                        }
+                        var dataUri = that.dataUri;
+                        if (dataUri.endsWith('/info.json')) {
+                            dataUri = dataUri.substr(0, dataUri.lastIndexOf('/'));
+                        }
+                        // if the request was redirected to a degraded version and there's a login service to get the full quality version
+                        if (uri !== dataUri && that.loginService) {
+                            that.status = HTTPStatusCode.MOVED_TEMPORARILY;
+                        }
+                        else {
+                            that.status = HTTPStatusCode.OK;
+                        }
+                        resolve(that);
+                    }
+                }).fail(function (error) {
+                    that.status = error.status;
+                    that.error = error;
+                    if (error.responseJSON) {
+                        that._parseAuthServices(error.responseJSON);
+                    }
+                    resolve(that);
+                });
+            });
+        };
+        return ExternalResource;
+    }());
+    Manifold.ExternalResource = ExternalResource;
+})(Manifold || (Manifold = {}));
+
+var Manifold;
+(function (Manifold) {
     var Helper = (function () {
         function Helper(options) {
             this.iiifResource = options.iiifResource;
