3dc79fe96ec992bf2a56c96cb693b377011afb35
edsilv <e.silverton@gmail.com>
2016-08-11 13:40:20 +0100

    readonly

--- dist/uv-1.8.15/lib/embed.js
+++ dist/uv-1.8.15/lib/embed.js
@@ -1,4 +1,4 @@
-// uv-1.8.14
+// uv-1.8.15
 
 //https://raw.githubusercontent.com/jfriend00/docReady/master/docready.js
 (function(funcName, baseObj) {
--- src/modules/uv-dialogues-module/ShareDialogue.ts
+++ src/modules/uv-dialogues-module/ShareDialogue.ts
@@ -79,7 +79,7 @@ class ShareDialogue extends Dialogue {
         this.$shareView = $('<div class="shareView"></div>');
         this.$tabsContent.append(this.$shareView);
 
-        this.$url = $('<input class="url" type="text" />');
+        this.$url = $('<input class="url" type="text" readonly="true" />');
         this.$shareView.append(this.$url);
 
         this.$embedView = $('<div class="embedView"></div>');
@@ -91,7 +91,7 @@ class ShareDialogue extends Dialogue {
         // this.$image = $('<img class="share" />');
         // this.$embedView.append(this.$image);
 
-        this.$code = $('<input class="code" type="text" />');
+        this.$code = $('<input class="code" type="text" readonly="true" />');
         this.$embedView.append(this.$code);
 
         this.$customSize = $('<div class="customSize"></div>');
