38917f592a9bb65ac72898afa2514a439805c025
edsilv <e.silverton@gmail.com>
2016-09-29 15:18:18 +0100

    merged mobile footer

--- src/_Version.ts
+++ src/_Version.ts
@@ -1,1 +1,1 @@
-export var Version = '1.8.29';
+export var Version = '1.8.30';
--- src/lib/embed.js
+++ src/lib/embed.js
@@ -1,4 +1,4 @@
-// uv-1.8.29
+// uv-1.8.30
 
 //https://raw.githubusercontent.com/jfriend00/docReady/master/docready.js
 (function(funcName, baseObj) {
