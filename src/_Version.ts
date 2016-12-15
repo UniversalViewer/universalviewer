b2ae97993a62345e5e892c488ac34473ac605785
edsilv <e.silverton@gmail.com>
2016-12-15 17:22:29 +0000

    bumped version

--- src/_Version.ts
+++ src/_Version.ts
@@ -1,1 +1,1 @@
-export var Version = '1.8.45';
+export var Version = '1.8.46';
--- src/lib/embed.js
+++ src/lib/embed.js
@@ -1,4 +1,4 @@
-// uv-1.8.45
+// uv-1.8.46
 
 //https://raw.githubusercontent.com/jfriend00/docReady/master/docready.js
 (function(funcName, baseObj) {
