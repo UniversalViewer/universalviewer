7bb854d16306f326945e0b2a01a9d0e88b39e4a1
Jack Reed <mejackreed@users.noreply.github.com>
2018-03-26 07:19:35 -0600

    Adds unit + integration testing to UV (#582)
    * add required dev dependency to dev dependencies
    
    * move testing to use jest + puppeteer and actually run on travis
    
    * move to __tests__ for jest default expection
    
    * cleanup travis.yml cruft
    
    * remove unused test dependencies mocha and selenium

---
+++ __tests__/test.js
@@ -0,0 +1,18 @@
+describe('Universal Viewer', () => {
+  beforeAll(async () => {
+    await page.goto('http://localhost:4444/examples');
+  });
+  it('has the correct page title', async () => {
+    const title = await page.title();
+    expect(title).toBe('Universal Viewer Examples');
+  });
+  it('loads the viewer images', async () => {
+    await page.waitForSelector('#thumb0');
+    const imageSrc = await page.$eval('#thumb0 img', e => e.src);
+    expect(imageSrc).toEqual(
+      expect.stringContaining(
+        'https://dlcs.io/iiif-img/wellcome/1/ff2085d5-a9c7-412e-9dbe-dda87712228d/full/90,/0/default.jpg?t='
+      )
+    );
+  });
+});
