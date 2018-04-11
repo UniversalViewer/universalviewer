1c4c3fb846083a1c87eedfedbdf206c73a18f3bd
Jack Reed <phillipjreed@gmail.com>
2018-04-11 15:53:30 -0600

    add tests around configuration of the uv thumbnail cache invalidation technique

---
+++ __tests__/configuration_options.js
@@ -0,0 +1,32 @@
+describe('Configuration options', () => {
+  describe('thumb cache invaldiation', () => {
+    beforeEach(async () => {
+      await page.goto('http://localhost:4444/examples');
+      await page.waitForSelector('#thumb0');
+    });
+    it('when set to false does not provide timestamp', async () => {
+      await page.evaluate(() => uv.set(
+        { config: { modules: { contentLeftPanel: { options: { thumbsCacheInvalidation: { enabled: false }}}}}}
+      ));
+      await page.waitForSelector('#thumb0');
+      const imageSrc = await page.$eval('#thumb0 img', e => e.src);
+      expect(imageSrc).toEqual( 
+        expect.stringMatching(
+          'https://dlcs.io/iiif-img/wellcome/1/ff2085d5-a9c7-412e-9dbe-dda87712228d/full/90,/0/default.jpg'
+        )
+      );
+    });
+    it('has a configurable parameter type', async () => {
+      await page.evaluate(() => uv.set(
+        { config: { modules: { contentLeftPanel: { options: { thumbsCacheInvalidation: { paramType: '#' }}}}}}
+      ));
+      await page.waitForSelector('#thumb0');
+      const imageSrc = await page.$eval('#thumb0 img', e => e.src);
+      expect(imageSrc).toEqual( 
+        expect.stringContaining(
+          'https://dlcs.io/iiif-img/wellcome/1/ff2085d5-a9c7-412e-9dbe-dda87712228d/full/90,/0/default.jpg#t='
+        )
+      );
+    });
+  });
+});
