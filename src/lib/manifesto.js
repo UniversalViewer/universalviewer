c1c91b02709580aa8cdc7d66c81866bf62e1968b
edsilv <e.silverton@gmail.com>
2018-05-18 15:54:00 +0100

    updated manifesto

--- src/lib/manifesto.js
+++ src/lib/manifesto.js
@@ -1,4 +1,4 @@
-// manifesto v2.2.21 https://github.com/iiif-commons/manifesto
+// manifesto v2.2.22 https://github.com/iiif-commons/manifesto
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.manifesto = f()}})(function(){var define,module,exports;return (function(){function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s}return e})()({1:[function(require,module,exports){
 (function (global){
 
@@ -1147,8 +1147,12 @@ var Manifesto;
             return _this;
         }
         Manifest.prototype.getBehavior = function () {
-            if (this.getProperty('behavior')) {
-                return new Manifesto.Behavior(this.getProperty('behavior'));
+            var behavior = this.getProperty('behavior');
+            if (Array.isArray(behavior)) {
+                behavior = behavior[0];
+            }
+            if (behavior) {
+                return new Manifesto.Behavior(behavior);
             }
             return null;
         };
@@ -1499,8 +1503,12 @@ var Manifesto;
             return this._ranges = this.items.en().where(function (m) { return m.isRange(); }).toArray();
         };
         Range.prototype.getBehavior = function () {
-            if (this.getProperty('behavior')) {
-                return new Manifesto.Behavior(this.getProperty('behavior'));
+            var behavior = this.getProperty('behavior');
+            if (Array.isArray(behavior)) {
+                behavior = behavior[0];
+            }
+            if (behavior) {
+                return new Manifesto.Behavior(behavior);
             }
             return null;
         };
