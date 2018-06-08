be06011803f3e628547b17357a73ac7e809c8f46
edsilv <e.silverton@gmail.com>
2018-06-08 11:44:20 +0100

    updated iiif-tree-component and iiif-av-component

--- src/extensions/uv-av-extension/lib/iiif-av-component.js
+++ src/extensions/uv-av-extension/lib/iiif-av-component.js
@@ -1,4 +1,4 @@
-// iiif-av-component v0.0.47 https://github.com/iiif-commons/iiif-av-component#readme
+// iiif-av-component v0.0.48 https://github.com/iiif-commons/iiif-av-component#readme
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.iiifAvComponent = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 (function (global){
 
@@ -103,8 +103,8 @@ var IIIFComponents;
                     console.warn('range not found');
                 }
                 else {
-                    if (range.canvases) {
-                        var canvasId = Manifesto.Utils.normaliseUrl(range.canvases[0]);
+                    var canvasId = IIIFComponents.AVComponentUtils.Utils.getFirstTargetedCanvasId(range);
+                    if (canvasId) {
                         // get canvas by normalised id (without temporal part)
                         var canvasInstance = this._getCanvasInstanceById(canvasId);
                         if (canvasInstance) {
@@ -244,7 +244,7 @@ var IIIFComponents;
                     }
                 }
             }
-            return null;
+            return undefined;
         };
         // private _getCurrentRange(): AVComponentObjects.CanvasRange | null {
         //     if (!this._data.helper || !this._data.helper.rangeId) {
@@ -261,7 +261,7 @@ var IIIFComponents;
             if (this._data.canvasId) {
                 return this._getCanvasInstanceById(this._data.canvasId);
             }
-            return null;
+            return undefined;
         };
         AVComponent.prototype._rewind = function () {
             if (this._data.limitToRange) {
@@ -1472,6 +1472,22 @@ var IIIFComponents;
                     t = temporal[1].split(',');
                 }
                 return t;
+            };
+            Utils.getFirstTargetedCanvasId = function (range) {
+                var canvasId;
+                if (range.canvases && range.canvases.length) {
+                    canvasId = range.canvases[0];
+                }
+                else {
+                    var childRanges = range.getRanges();
+                    if (childRanges.length) {
+                        return Utils.getFirstTargetedCanvasId(childRanges[0]);
+                    }
+                }
+                if (canvasId !== undefined) {
+                    return Manifesto.Utils.normaliseUrl(canvasId);
+                }
+                return undefined;
             };
             Utils.getRangeDuration = function (range) {
                 var start;
