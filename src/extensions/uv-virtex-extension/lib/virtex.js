24015dc5602f1f35a474740926a5b05e33f04121
edsilv <e.silverton@gmail.com>
2018-06-12 16:50:58 +0100

    updated virtex

--- src/extensions/uv-virtex-extension/lib/virtex.js
+++ src/extensions/uv-virtex-extension/lib/virtex.js
@@ -1,4 +1,4 @@
-// virtex v0.3.12 https://github.com/edsilv/virtex#readme
+// virtex v0.3.14 https://github.com/edsilv/virtex#readme
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.virtex = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 (function (global){
 
@@ -148,17 +148,10 @@ var Virtex;
                 // https://github.com/donmccurdy/three-gltf-viewer/blob/master/src/viewer.js#L183
                 // allow specifying envmap? https://github.com/mrdoob/three.js/blob/dev/examples/webgl_loader_gltf.html#L92
                 var obj = gltf.scene || gltf.scenes[0];
-                // obj.traverse(function (node: any) {
-                //     if (node.isMesh) {
-                //         console.log('change encoding');
-                //         if (node.material.map) node.material.map.encoding = THREE.sRGBEncoding;
-                //         if (node.material.emissiveMap) node.material.emissiveMap.encoding = THREE.sRGBEncoding;
-                //     }
-                // });
-                viewport.options.data.ambientLightIntensity = 0.05;
-                viewport.options.data.directionalLight1Intensity = 0.5;
-                viewport.options.data.directionalLight2Intensity = 0.1;
-                // https://github.com/mrdoob/three.js/pull/12766
+                viewport.options.data.ambientLightIntensity = 0.2;
+                viewport.options.data.directionalLight1Intensity = 0.75;
+                viewport.options.data.directionalLight2Intensity = 0.2;
+                // https://github.com/mrdoob/three.js/issues/12554
                 viewport.renderer.gammaOutput = true;
                 viewport.objectGroup.add(obj);
                 viewport.createLights();
