ed9a41df8e848c9d4ff2f4f7c9ec1aeffcb0e605
edsilv <e.silverton@gmail.com>
2018-08-23 22:48:59 +0100

    position attribution

--- src/modules/uv-avcenterpanel-module/AVCenterPanel.ts
+++ src/modules/uv-avcenterpanel-module/AVCenterPanel.ts
@@ -129,7 +129,7 @@ export class AVCenterPanel extends CenterPanel {
             
         }, false);
 
-        this._whenMediaReady(() => {
+        this.whenResized(() => {
             this.attributionPosition = Position.BOTTOM_RIGHT;
             this.updateRequiredStatement();
         });
