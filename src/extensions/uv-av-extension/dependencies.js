8879ce562017620ea422afed47166f768983c772
Edward Silverton <e.silverton@gmail.com>
2018-09-11 17:00:43 +0100

    build

--- src/extensions/uv-av-extension/dependencies.js
+++ src/extensions/uv-av-extension/dependencies.js
@@ -1,45 +1,64 @@
 define(function () {
-    function isSafari() {
-        var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
-        return isSafari;
-    }
-    function isAdaptiveStreamingAvailable() {
-        var isAvailable = !!(window.MediaSource || window.WebKitMediaSource);
-        return isAvailable;
-    }
+    // https://developer.mozilla.org/en-US/Apps/Fundamentals/Audio_and_video_delivery/Live_streaming_web_audio_and_video
+    // Dash is supported everywhere except safari
+    // function isSafari() {
+    //     // https://stackoverflow.com/questions/7944460/detect-safari-browser?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+    //     var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+    //     console.log('isSafari', isSafari);
+    //     return isSafari;
+    // }
     function isFormatAvailable(formats, format) {
         var isAvailable = formats.includes(format);
+        console.log('isFormatAvailable', format, isAvailable);
         return isAvailable;
     }
-    function isHLSAvailable(formats) {
-        return isFormatAvailable(formats, 'application/vnd.apple.mpegurl');
+    function isHLSFormatAvailable(formats) {
+        return isFormatAvailable(formats, 'application/vnd.apple.mpegurl') || isFormatAvailable(formats, 'vnd.apple.mpegurl');
     }
-    function isMpegDashAvailable(formats) {
+    function isMpegDashFormatAvailable(formats) {
         return isFormatAvailable(formats, 'application/dash+xml');
+    }
+    function canPlayHls() {
+        var doc = typeof document === 'object' && document, videoelem = doc && doc.createElement('video'), isvideosupport = Boolean(videoelem && videoelem.canPlayType), 
+        // HLS manifests can go by many mime-types
+        canPlay = [
+            // Apple santioned
+            'application/vnd.apple.mpegurl',
+            // Apple sanctioned for backwards compatibility
+            'audio/mpegurl',
+            // Very common
+            'audio/x-mpegurl',
+            // Very common
+            'application/x-mpegurl',
+            // Included for completeness
+            'video/x-mpegurl',
+            'video/mpegurl',
+            'application/mpegurl'
+        ];
+        return isvideosupport && canPlay.some(function (canItPlay) {
+            return /maybe|probably/i.test(videoelem.canPlayType(canItPlay));
+        });
     }
     return function (formats) {
         var alwaysRequired = ['iiif-tree-component', 'iiif-av-component', 'iiif-metadata-component', 'jquery-ui.min', 'jquery.ui.touch-punch.min', 'jquery.binarytransport', 'waveform-data'];
-        if (isAdaptiveStreamingAvailable()) {
-            if (isMpegDashAvailable(formats) && !isSafari()) {
-                return {
-                    sync: alwaysRequired.concat(['dash.all.min'])
-                };
-            }
-            else if (isHLSAvailable(formats)) {
-                return {
-                    sync: alwaysRequired.concat(['hls.min'])
-                };
-            }
-            else {
-                return {
-                    sync: alwaysRequired
-                };
-            }
+        if (isHLSFormatAvailable(formats) && canPlayHls()) {
+            console.log('load HLS');
+            return {
+                sync: alwaysRequired.concat(['hls.min'])
+            };
+        }
+        else if (isMpegDashFormatAvailable(formats)) {
+            console.log('load mpeg dash');
+            return {
+                sync: alwaysRequired.concat(['dash.all.min'])
+            };
         }
         else {
+            console.log('adaptive streaming not available');
             return {
                 sync: alwaysRequired
             };
         }
     };
 });
+//# sourceMappingURL=dependencies.js.map
--- src/extensions/uv-mediaelement-extension/dependencies.js
+++ src/extensions/uv-mediaelement-extension/dependencies.js
@@ -5,3 +5,4 @@ define(function () {
         };
     };
 });
+//# sourceMappingURL=dependencies.js.map
