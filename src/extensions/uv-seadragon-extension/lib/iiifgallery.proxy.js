0c26594b7ba171826b19692e8af188f4c86e9e7f
edsilv <e.silverton@gmail.com>
2018-11-09 11:51:01 +0000

    updated jest

--- src/extensions/uv-seadragon-extension/lib/iiifgallery.proxy.js
+++ src/extensions/uv-seadragon-extension/lib/iiifgallery.proxy.js
@@ -1,7 +1,8 @@
+// demonstrates how to load a stencil component via a proxy script
 // https://github.com/ionic-team/stencil/issues/365
-(function() {
-    var t = document.createElement('script');
-    t.type = 'text/javascript';
-    t.src = 'uv/lib/iiifgallery.js';
-    document.body.appendChild(t);
-})();
+// (function() {
+//     var t = document.createElement('script');
+//     t.type = 'text/javascript';
+//     t.src = 'uv/lib/iiifgallery.js';
+//     document.body.appendChild(t);
+// })();
