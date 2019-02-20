ff649187bba94347ca74d508b479ed45b84ffb2b
stephenwf <stephen.fraser@digirati.com>
2019-02-20 16:18:47 +0000

    Added support in blacklist for our custom attribute

--- src/Utils.ts
+++ src/Utils.ts
@@ -1,46 +1,45 @@
-import { IUVData } from "./IUVData";
+import { IUVData } from './IUVData';
 
 declare var filterXSS: any;
 
 export class UVUtils {
+	static sanitize(html: string): string {
+		return filterXSS(html, {
+			whiteList: {
+				b: [],
+				a: [ 'href', 'title', 'target', 'class', 'data-uv-navigate' ],
+				br: [],
+				i: [],
+				img: [ 'src' ],
+				p: [],
+				small: [],
+				sub: [],
+				sup: [],
+				span: [ 'data-uv-navigate' ]
+			}
+		});
+	}
 
-    static sanitize(html: string): string {
-        return filterXSS(html, {
-            whiteList: {
-                a: ["href", "title", "target", "class"],
-                b: [],
-                br: [],
-                i: [],
-                img: ["src"],
-                p: [],
-                small: [],
-                span: [],
-                sub: [],
-                sup: []
-            }
-        });
-    }
+	static isValidUrl(value: string): boolean {
+		const a = document.createElement('a');
+		a.href = value;
+		return !!a.host && a.host !== window.location.host;
+	}
 
-    static isValidUrl(value: string): boolean {
-        const a = document.createElement('a');
-        a.href = value;
-        return (!!a.host && a.host !== window.location.host);
-    }
+	static propertiesChanged(newData: IUVData, currentData: IUVData, properties: string[]): boolean {
+		let propChanged: boolean = false;
 
-    static propertiesChanged(newData: IUVData, currentData: IUVData, properties: string[]): boolean {
-        let propChanged: boolean = false;
-        
-        for (var i = 0; i < properties.length; i++) {
-            propChanged = UVUtils.propertyChanged(newData, currentData, properties[i]);
-            if (propChanged) {
-                break;
-            }
-        }
+		for (var i = 0; i < properties.length; i++) {
+			propChanged = UVUtils.propertyChanged(newData, currentData, properties[i]);
+			if (propChanged) {
+				break;
+			}
+		}
 
-        return propChanged;
-    }
+		return propChanged;
+	}
 
-    static propertyChanged(newData: IUVData, currentData: IUVData, propertyName: string): boolean {
-        return newData[propertyName] !== undefined && currentData[propertyName] !== newData[propertyName];
-    }
+	static propertyChanged(newData: IUVData, currentData: IUVData, propertyName: string): boolean {
+		return newData[propertyName] !== undefined && currentData[propertyName] !== newData[propertyName];
+	}
 }
