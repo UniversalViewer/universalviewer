67994937e56eeed8b21d7c81cf16c0d262f77801
edsilv <e.silverton@gmail.com>
2019-03-13 14:16:15 +0000

    updated av component

--- src/extensions/uv-virtex-extension/dependencies.js
+++ src/extensions/uv-virtex-extension/dependencies.js
@@ -3,6 +3,8 @@ define(function () {
         return {
             sync: ['three.min'],
             async: ['VRControls', 'VREffect', 'stats.min', 'OBJLoader', 'MTLLoader', 'PLYLoader', 'GLTFLoader', 'Detector', 'WebVR', 'virtex', 'MetadataComponent']
+            // sync: ['three.min', 'draco_decoder'],
+            // async: ['VRControls', 'VREffect', 'stats.min', 'DRACOLoader', 'GLTFLoader', 'OBJLoader', 'MTLLoader', 'Detector', 'WebVR', 'virtex', 'iiif-metadata-component']
         };
     };
 });
