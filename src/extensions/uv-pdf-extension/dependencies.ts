248b2c62123198d74a10c848a038a38bf9badef6
Geoff <geoffrey.scholl@villanova.edu>
2019-04-02 14:59:55 -0400

    Added PDFObject with config setting

--- src/extensions/uv-pdf-extension/dependencies.ts
+++ src/extensions/uv-pdf-extension/dependencies.ts
@@ -1,6 +1,7 @@
 define(function() {
     return function(formats: string[]) {
         return {
+            sync: ['pdfobject'],
             async: ['!pdfjs-dist/build/pdf.combined', 'MetadataComponent']
         };
     }
