a170176457d29bf8985cf449cf004ca4698d989e
Geoff Scholl <gscholl@villanova.edu>
2019-07-05 09:24:37 -0400

    usePdfJs config - hide header panel for pdfobject (#675)
    * usePdfJs config - hide header panel for pdfobject
    
    * Update Extension.ts
    
    * Update PDFCenterPanel.ts
    
    * Update Extension.ts

--- src/extensions/uv-pdf-extension/Extension.ts
+++ src/extensions/uv-pdf-extension/Extension.ts
@@ -84,6 +84,10 @@ export class Extension extends BaseExtension implements IPDFExtension {
         return false;
     }
 
+    isHeaderPanelEnabled(): boolean {
+        return super.isHeaderPanelEnabled() && Utils.Bools.getBool(this.data.config.options.usePdfJs, true);
+    }
+
     createModules(): void{
         super.createModules();
 
