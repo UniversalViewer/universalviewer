9b6e597bf011ad9e2935368dc991cefa5fdc42a2
Edward Silverton <e.silverton@gmail.com>
2020-03-13 13:28:15 +0000

    remove uv.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,6 @@ function resolvePath(p) {
 }
 
 const config = {
-    // These are the entry point of our library. We tell webpack to use the name we assign later, when creating the bundle.
-    // We also use the name to filter the second entry point for applying code minification via UglifyJS
     entry: {
         "UV": ["./src/index.ts"]
     },
@@ -16,8 +14,6 @@ const config = {
         "fetch-cookie": "fetch",
         "tough-cookie": "fetch"
     },
-    // The output defines how and where we want the bundles. The special value `[name]` in `filename` tells Webpack to use the name we defined above.
-    // We target a UMD and name it UV. When including the bundle in the browser it will be accessible at `window.UV`
     output: {
         path: resolvePath("uv-dist-umd"),
         publicPath: "uv-dist-umd/",
@@ -26,12 +22,9 @@ const config = {
         umdNamedDefine: true,
         chunkFilename: "[name].[contenthash].js"
     },
-    // Add resolve for `tsx` and `ts` files, otherwise Webpack would
-    // only look for common JavaScript file extension (.js)
     resolve: {
         extensions: [".ts", ".tsx", ".js"]
     },
-    // Webpack doesn't understand TypeScript files and a loader is needed.
     module: {
         rules: [
             {
