1b6db2495c950129554284987dde8259a50a62d0
Shaukat Ali <mohammad.ali@niit-tech.com>
2020-03-24 10:16:26 +0000

    ProgressDialog module created

---
+++ src/modules/uv-dialogues-module/ProgressDialogue.ts
@@ -0,0 +1,61 @@
+import {Dialogue} from "../uv-shared-module/Dialogue";
+
+export class ProgressDialogue extends Dialogue {
+
+    $label: JQuery;
+    $progressBar: JQuery;
+    value: number = 0;
+
+    constructor($element: JQuery) {
+        super($element);
+    }
+
+    create(): void {
+
+        this.setConfig('progressDialogue');
+        super.create();
+
+        //setting default options
+        this.options = $.extend(true, this.options, {
+            label: "Please wait...",
+            maxValue: 100,
+            showPercentage: false
+        });
+
+        this.disableClose();
+        this.$buttons.remove();
+        
+        this.$label = $(`<label>${ this.options.label }</label>`);
+        this.$content.append(this.$label);
+
+        this.$progressBar = $(`<progress class="progress-bar" max="${this.options.maxValue}" value="0"></progress>`);
+        this.$content.append(this.$progressBar);
+
+        this.$element.hide();
+    }
+
+    setOptions(options:any): void{
+        this.options = $.extend(true, this.options, options);
+
+        //reset value
+        this.value = 0;
+
+        //reset content
+        this.$label.html(this.options.label);
+        this.$progressBar.attr('max', this.options.maxValue);
+        this.$progressBar.attr('value', this.value);
+    }
+
+    setValue(value:number):void{
+        this.value = value;
+        this.$progressBar.attr('value', this.value);
+    }
+
+    open(): void {
+        super.open();
+    }
+
+    resize(): void {
+        super.resize();
+    }
+}
