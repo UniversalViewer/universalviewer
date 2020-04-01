65ca0fb5cd8565c374182419e4a80f312419d424
Edward Silverton <e.silverton@gmail.com>
2020-04-01 16:24:21 +0100

    include webcomponents polyfill for modelviewer

--- src/modules/uv-modelviewercenterpanel-module/ModelViewerCenterPanel.ts
+++ src/modules/uv-modelviewercenterpanel-module/ModelViewerCenterPanel.ts
@@ -1,3 +1,4 @@
+import "@webcomponents/webcomponentsjs/webcomponents-bundle.js";
 import "../../../node_modules/@google/model-viewer/dist/model-viewer-legacy";
 import { AnnotationBody, Canvas, IExternalResource } from "manifesto.js";
 import { sanitize } from "../../Utils";
