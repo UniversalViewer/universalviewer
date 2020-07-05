fbcbbde780bd73aea0496df6f2d96198ac5c7efb
Edward Silverton <e.silverton@gmail.com>
2020-07-05 13:34:04 +0100

    use UV.init

--- config.js
+++ config.js
@@ -3,7 +3,6 @@ var metadata = require('./package');
 module.exports = function () {
     this.name = metadata.name;
     this.header = '// ' + this.name + ' v' + metadata.version + ' ' + metadata.homepage + '\n';
-    this.wwwPort = 8002;
     this.dependencies = {
         // goes in assets/js/
         bundle: [
@@ -18,7 +17,6 @@ module.exports = function () {
         distumd: './uv-dist-umd',
         www: './www',
         extensions: './src/extensions',
-        ionic: './node_modules/@ionic/core/dist',
         lib: './src/lib',
         modules: './src/modules',
         npm: './node_modules',
