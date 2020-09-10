2bfa5a80496d4d596d99cb269ef681938e0675fe
Chris Hallberg <crhallberg@gmail.com>
2020-09-10 13:02:08 -0400

    Dropdown focus close now accounts for delayed opening.

--- src/modules/uv-shared-module/AutoComplete.ts
+++ src/modules/uv-shared-module/AutoComplete.ts
@@ -133,9 +133,14 @@ export class AutoComplete {
     });
 
     // hide results if focus moves on.
-    this._$element.on("focusout", e => {
-      this._clearResults();
-      this._hideResults();
+    $(document).on("focusin", e => {
+      if (
+        this._$searchResultsList.has($(e.target)[0]).length === 0 &&
+        !this._$element.is($(e.target)[0])
+      ) {
+        this._clearResults();
+        this._hideResults();
+      }
     });
 
     this._hideResults();
