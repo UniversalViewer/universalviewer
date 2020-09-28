a1df6a14f245fb8387062197dedc4af87994d912
Stephen Fraser <stephen.fraser@digirati.com>
2020-09-28 16:27:47 +0100

    Dependency name changes

--- src/extensions/uv-seadragon-extension/dependencies.js
+++ src/extensions/uv-seadragon-extension/dependencies.js
@@ -1,7 +1,9 @@
 define(function () {
     return function (formats) {
         return {
-            async: ['TreeComponent', 'GalleryComponent', 'MetadataComponent', 'openseadragon.min']
+            async: ['TreeComponent', 'GalleryComponent', 'IIIFMetadataComponent', 'openseadragon.min']
+            //async: ['TreeComponent', 'iiifgallery.proxy', 'GalleryComponent', 'MetadataComponent', 'openseadragon.min']
         };
     };
 });
+//# sourceMappingURL=dependencies.js.map
--- src/extensions/uv-virtex-extension/dependencies.js
+++ src/extensions/uv-virtex-extension/dependencies.js
@@ -2,7 +2,10 @@ define(function () {
     return function (formats) {
         return {
             sync: ['three.min'],
-            async: ['VRControls', 'VREffect', 'stats.min', 'OBJLoader', 'MTLLoader', 'PLYLoader', 'GLTFLoader', 'Detector', 'WebVR', 'virtex', 'MetadataComponent']
+            async: ['VRControls', 'VREffect', 'stats.min', 'OBJLoader', 'MTLLoader', 'PLYLoader', 'GLTFLoader', 'Detector', 'WebVR', 'virtex', 'IIIFMetadataComponent']
+            // sync: ['three.min', 'draco_decoder'],
+            // async: ['VRControls', 'VREffect', 'stats.min', 'DRACOLoader', 'GLTFLoader', 'OBJLoader', 'MTLLoader', 'Detector', 'WebVR', 'virtex', 'iiif-metadata-component']
         };
     };
 });
+//# sourceMappingURL=dependencies.js.map
