e41491939c9e207d92242a1895c6211e672dbd2f
Stephen Fraser <stephen.fraser@digirati.com>
2021-11-02 15:29:32 +0000

    Extra URL adapter helper

--- src/URLAdaptor.ts
+++ src/URLAdaptor.ts
@@ -1,13 +1,14 @@
 import { UVAdaptor } from "./UVAdaptor";
 import { Urls } from "@edsilv/utils";
+import {UniversalViewer} from "./UniversalViewer";
 
 export class URLAdaptor extends UVAdaptor {
   constructor(readonly: boolean = false) {
     super(readonly);
   }
 
-  public get(key: string, defaultValue: string | null): string | null {
-    return Urls.getHashParameter(key, document) || defaultValue;
+  public get(key: string, defaultValue?: string | null): string | null {
+    return Urls.getHashParameter(key, document) || defaultValue || null;
   }
 
   public getFragment(key: string, url: string): string | null {
@@ -16,7 +17,7 @@ export class URLAdaptor extends UVAdaptor {
     return(match ? decodeURIComponent(match[1].replace(/\+/g, " ")) : null);
   }
 
-  public set(key: string, value: string): void {
+  public set(key: string, value: string | number | null): void {
     if (!this.readonly) {
       if (value) {
         Urls.setHashParameter(key, value.toString(), document);
@@ -24,5 +25,63 @@ export class URLAdaptor extends UVAdaptor {
         Urls.setHashParameter(key, "", document);
       }
     }
+  }
+
+  public getInitialData(defaults?: any) {
+    return {
+      ...(defaults || {}),
+      collectionIndex:
+          this.get("c") !== undefined
+              ? Number(this.get("c"))
+              : undefined,
+      manifestIndex: Number(this.get("m", 0)),
+      canvasIndex: Number(this.get("cv", 0)),
+      rotation: Number(this.get("r", 0)),
+      rangeId: this.get("rid", ""),
+      xywh: this.get("xywh", ""),
+      target: this.get("target", "")
+    };
+  }
+
+  public bindTo(uv: UniversalViewer) {
+    uv.on(
+        "collectionIndexChange",
+         (collectionIndex) => {
+          this.set("c", collectionIndex);
+        },
+        false
+    );
+
+    uv.on(
+        "manifestIndexChange",
+         (manifestIndex) => {
+          this.set("m", manifestIndex);
+        },
+        false
+    );
+
+    uv.on(
+        "canvasIndexChange",
+         (canvasIndex) => {
+          this.set("cv", canvasIndex);
+        },
+        false
+    );
+
+    uv.on(
+        "rangeChange",
+         (rangeId) => {
+          this.set("rid", rangeId);
+        },
+        false
+    );
+
+    uv.on(
+        "targetChange",
+        (target) => {
+        this.set("xywh", this.getFragment("xywh", target));
+      },
+      false
+    );
   }
 }
