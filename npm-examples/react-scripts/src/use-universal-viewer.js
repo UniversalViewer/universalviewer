8481073236ec7af6ae646f25020a051fdceea0c3
Stephen Fraser <stephen.fraser@digirati.com>
2021-11-03 15:23:13 +0000

    Added ignored files

---
+++ npm-examples/react-scripts/src/use-universal-viewer.js
@@ -0,0 +1,37 @@
+import { useLayoutEffect, useState } from 'react';
+import { init } from 'universalviewer';
+
+export function useEvent(viewer, name, cb) {
+    useLayoutEffect(() => {
+        if (viewer) {
+            return viewer.subscribe(name, cb);
+        }
+    }, [viewer])
+}
+
+export function useUniversalViewer(ref, options, resizeIfTheseChange = []) {
+    const [uv, setUv] = useState();
+
+    useLayoutEffect(() => {
+        if (uv) {
+            try {
+                ref.current.firstChild.style.width = ref.current.offsetWidth + "px";
+                ref.current.firstChild.style.height = ref.current.offsetHeight + "px";
+                uv.resize();
+            } catch (e) {
+                // This may fail if we are disposed.
+            }
+        }
+    }, resizeIfTheseChange)
+
+    useLayoutEffect(() => {
+        const currentUv = init(ref.current, options);
+        setUv(currentUv);
+
+        return () => {
+            currentUv.dispose();
+        }
+    }, []);
+
+    return uv;
+}
