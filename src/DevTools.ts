5490354d1086b875c5cc0cb1ffd780104562b624
Stephen Fraser <stephen.fraser@digirati.com>
2022-03-03 09:34:26 +0000

    Added start of simple dev tools

---
+++ src/DevTools.ts
@@ -0,0 +1,78 @@
+import {UniversalViewer} from "./UniversalViewer";
+import {BaseEvents} from "./modules/uv-shared-module/BaseEvents";
+
+export class DevTools {
+  element: HTMLElement;
+  uv: UniversalViewer;
+  log: HTMLTextAreaElement;
+  buttonContainer: HTMLDivElement;
+  buttons: Record<string, HTMLButtonElement>;
+  logging = false;
+  originalLogger: any;
+
+  constructor(element: HTMLElement, uv: UniversalViewer) {
+    this.element = element;
+    this.uv = uv;
+
+
+    const title = document.createElement('h2');
+    title.innerText = 'dev tools';
+    this.element.append(title);
+    this.log = document.createElement('textarea');
+    this.log.rows = 20;
+    this.log.disabled = true;
+    this.log.wrap = 'soft';
+    this.element.style.display = 'block';
+    this.element.style.clear = 'both';
+    this.log.style.whiteSpace = 'no-wrap';
+    this.log.style.overflowX = 'scroll';
+    this.log.style.whiteSpace = 'pre';
+    this.log.style.width = '100%';
+    this.element.append(this.log);
+
+    this.buttons = {};
+    this.buttonContainer = document.createElement('div');
+    this.element.append(this.buttonContainer);
+
+    this.createButton('Clear log', () => {
+      this.log.value = '';
+    })
+
+    const jsonStringify = (j: any) => {
+      try {
+        return JSON.stringify(j);
+      } catch (e) {
+        return `${j}`;
+      }
+    }
+
+    this.originalLogger = console.log.bind(console);
+    console.log = (...input: any[]) => {
+      this.log.value = this.log.value += input ? `\nconsole.log(${input.map(v => jsonStringify(v)).join(', ')})` : ``;
+      this.log.scrollTop = this.log.scrollHeight;
+      this.originalLogger(...input)
+    }
+
+    for (const ev of Object.keys(BaseEvents)) {
+      const event = BaseEvents[ev];
+      this.uv.on(event, (e, ...rest) => {
+        this.log.value = this.log.value += e ? `\n${ev}(${jsonStringify(e)})` : `\n${ev}`;
+        this.log.scrollTop = this.log.scrollHeight;
+        if (this.logging) {
+          this.originalLogger(ev, e, ...rest);
+        }
+      }, {})
+    }
+    this.uv.on(BaseEvents.CANVAS_INDEX_CHANGE, (ev) => {
+      console.log(ev);
+    }, {})
+
+  }
+
+  createButton(name: string, onClick: (e: MouseEvent) => void) {
+    this.buttons[name] = document.createElement('button');
+    this.buttons[name].innerText = name;
+    this.buttons[name].addEventListener('click', onClick);
+    this.buttonContainer.append(this.buttons[name]);
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,4 @@ export { UniversalViewer as Viewer } from "./UniversalViewer";
 export { Events } from "./Events";
 export { IIIFEvents } from "./content-handlers/iiif/IIIFEvents";
 export { init } from "./Init";
+export { DevTools } from './DevTools';
