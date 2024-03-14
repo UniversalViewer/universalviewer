393fe63185a2e69b4073391d62439566d3ef6508
Nicolas Franck <Nicolas.Franck@ugent.be>
2024-03-14 15:58:19 +0100

    Fix problem with "current view" breaking for v3 images (#971)

--- src/content-handlers/iiif/extensions/uv-openseadragon-extension/Extension.ts
+++ src/content-handlers/iiif/extensions/uv-openseadragon-extension/Extension.ts
@@ -1281,7 +1281,7 @@ export default class OpenSeadragonExtension extends BaseExtension<Config> {
 
   getImageId(canvas: Canvas): string | null {
     if (canvas.externalResource) {
-      const id: string | undefined = canvas.externalResource.data["@id"];
+      const id: string | undefined = canvas.externalResource.data["@id"] || canvas.externalResource.data.id;
 
       if (id) {
         return id.substr(id.lastIndexOf("/") + 1);
@@ -1339,7 +1339,7 @@ export default class OpenSeadragonExtension extends BaseExtension<Config> {
             id += "/";
           }
 
-          if (Utils.isImageProfile(service.getProfile())) {
+          if (Utils.isImageProfile(service.getProfile()) || Utils.isImageServiceType(service.getIIIFResourceType())) {
             infoUri = id + "info.json";
           }
         }
