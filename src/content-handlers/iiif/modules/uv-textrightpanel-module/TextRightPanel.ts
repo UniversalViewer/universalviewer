eda7915e7dec64b34966ef580a04166427e51a92
Anders Pettersson <landsfiskalen@gmail.com>
2024-04-16 13:21:14 +0200

    Sorted some bugs, and changed the hover colour for transcribed text.

--- src/content-handlers/iiif/modules/uv-textrightpanel-module/TextRightPanel.ts
+++ src/content-handlers/iiif/modules/uv-textrightpanel-module/TextRightPanel.ts
@@ -15,11 +15,12 @@ export class TextRightPanel extends RightPanel<TextRightPanelConfig> {
 
     super.create();
 
-    this.extensionHost.on(Events.LOAD, () => {
+    this.extensionHost.on(Events.LOAD, async () => {
       this.$main.html('');
       let canvases = this.extension.getCurrentCanvases();
       canvases.sort((a, b) => (a.index as number - b.index as number));
-      canvases.forEach((c, i) => {
+      for (let i = 0; i < canvases.length; i++) {
+        const c = canvases[i];
         let seeAlso = c.getProperty("seeAlso");
         let header;
         if (i === 0 && canvases.length > 1) {
@@ -31,14 +32,14 @@ export class TextRightPanel extends RightPanel<TextRightPanelConfig> {
         // and make sure which version of IIIF Presentation API is used
         if (seeAlso.length === undefined) { // This is IIIF Presentation API < 3
           if (seeAlso.profile.includes('alto')) {
-            this.processAltoFile(seeAlso['@id'], header);
+            await this.processAltoFile(seeAlso['@id'], header);
           }
         } else { // This is IIIF Presentation API 3
           if (seeAlso[0].profile.includes('alto')) {
-            this.processAltoFile(seeAlso[0]['id'], header);
+            await this.processAltoFile(seeAlso[0]['id'], header);
           }
         }
-      });
+      };
     });
 
     this.setTitle(this.config.content.title);
@@ -64,34 +65,33 @@ export class TextRightPanel extends RightPanel<TextRightPanelConfig> {
   }
 
   // Let's load the ALTO file and do some parsing
-  processAltoFile(altoUrl, header?): void {
-    fetch(altoUrl)
-      .then(response => response.text())
-      .then(data => {
-        const altoDoc = new DOMParser().parseFromString(data, 'application/xml');
-        const textLines = altoDoc.querySelectorAll('TextLine');
-        let lines = Array.from(textLines).map((e, i) => {
-          const strings = e.querySelectorAll('String');
-          var t = Array.from(strings).map((e, i) => {
-            return e.getAttribute("CONTENT")
-          });
-          let line = $('<p>' + t.join(' ') + '</p>');
-          return line;
-        })
-        this.$transcribedText = $('<div class="transcribed-text"></div>')
-        if (header) {
-          this.$transcribedText.append($('<div class="label">' + header + '</div>'));
-        }
-        if (lines.length > 0) {
-          this.$transcribedText.append(lines);
-        } else {
-          this.$transcribedText.append($('<div>' + this.content.textNotFound + '</div>'));
-        }
-        this.$main.append(this.$transcribedText);
-      })
-      .catch(() => {
-        throw new Error('Unable to fetch Alto file.')
+  processAltoFile = async (altoUrl, header?): Promise<void> => {
+    try {
+      const response = await fetch(altoUrl);
+      const data = await response.text();
+      const altoDoc = new DOMParser().parseFromString(data, 'application/xml');
+      const textLines = altoDoc.querySelectorAll('TextLine');
+      let lines = Array.from(textLines).map((e, i) => {
+        const strings = e.querySelectorAll('String');
+        var t = Array.from(strings).map((e, i) => {
+          return e.getAttribute("CONTENT")
+        });
+        let line = $('<p>' + t.join(' ') + '</p>');
+        return line;
       });
+      this.$transcribedText = $('<div class="transcribed-text"></div>');
+      if (header) {
+        this.$transcribedText.append($('<div class="label">' + header + '</div>'));
+      }
+      if (lines.length > 0) {
+        this.$transcribedText.append(lines);
+      } else {
+        this.$transcribedText.append($('<div>' + this.content.textNotFound + '</div>'));
+      }
+      this.$main.append(this.$transcribedText);
+    } catch (error) {
+      throw new Error('Unable to fetch Alto file: ' + error.message);
+    }
   }
 
 }
