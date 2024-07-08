14de5c65e094d2c0c299627849dac1b79c922012
James Misson <71402529+jamesmisson@users.noreply.github.com>
2024-07-08 17:11:26 +0100

    Activate next result button on single canvas

--- src/content-handlers/iiif/modules/uv-searchfooterpanel-module/FooterPanel.ts
+++ src/content-handlers/iiif/modules/uv-searchfooterpanel-module/FooterPanel.ts
@@ -361,7 +361,6 @@ export class FooterPanel extends BaseFooterPanel<
     const currentCanvasIndex: number = this.extension.helper.canvasIndex;
     const lastSearchResultCanvasIndex: number = this.getLastSearchResultCanvasIndex();
     const currentSearchResultRectIndex: number = this.getCurrentSearchResultRectIndex();
-    const totalCanvases: number = this.extension.helper.getTotalCanvases();
 
     // if zoom to search result is enabled and there is a highlighted search result.
     if (
@@ -381,11 +380,7 @@ export class FooterPanel extends BaseFooterPanel<
       return true;
     }
 
-    // if only one canvas then allow clicking next result
-    if (totalCanvases === 1) {
-      return true}
-
-    return currentCanvasIndex < lastSearchResultCanvasIndex;
+    return currentCanvasIndex <= lastSearchResultCanvasIndex;
   }
 
   getSearchResults(): AnnotationGroup[] {
