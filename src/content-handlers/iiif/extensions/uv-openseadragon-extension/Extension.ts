c70100fa5c9bcce3c281f5cfc4d2f266d6ed73bf
LlGC-jop <jop@llgc.org.uk>
2024-10-21 14:02:19 +0100

    copy update -> change settings from osd ext to base so all use it

--- src/content-handlers/iiif/extensions/uv-openseadragon-extension/Extension.ts
+++ src/content-handlers/iiif/extensions/uv-openseadragon-extension/Extension.ts
@@ -483,8 +483,6 @@ export default class OpenSeadragonExtension extends BaseExtension<Config> {
         IIIFEvents.CANVAS_INDEX_CHANGE,
         this.helper.canvasIndex
       );
-      const settings: ISettings = this.getSettings();
-      this.extensionHost.publish(IIIFEvents.SETTINGS_CHANGE, settings);
     });
 
     this.extensionHost.subscribe(
@@ -1373,14 +1371,12 @@ export default class OpenSeadragonExtension extends BaseExtension<Config> {
     const config: string = this.data.config!.uri || "";
     const locales: string | null = this.getSerializedLocales();
     const appUri: string = this.getAppUri();
-    const title: string = this.helper.getLabel() || "";
     const iframeSrc: string = `${appUri}#?manifest=${this.helper.manifestUri}&c=${this.helper.collectionIndex}&m=${this.helper.manifestIndex}&cv=${this.helper.canvasIndex}&config=${config}&locales=${locales}&xywh=${zoom}&r=${rotation}`;
     const script: string = Strings.format(
       template,
       iframeSrc,
       width.toString(),
-      height.toString(),
-      title
+      height.toString()
     );
     return script;
   }
