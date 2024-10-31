8f8a2947e20c92d3b0d89e1ef061741667485914
Jonathan Phillips <125897079+LlGC-jop@users.noreply.github.com>
2024-10-31 14:20:37 +0000

    Improve info panel focusability (fixes #1069) (#1171)
    * Adds a tabindex to the right info panel's main element so it can be focused regardless of whether it has focus-able content
    
    * check for scrolling before setting/unsetting tabindex and aria label. make elements articles so they'll be readable by screenreaders.

--- src/content-handlers/iiif/modules/uv-dialogues-module/MoreInfoDialogue.ts
+++ src/content-handlers/iiif/modules/uv-dialogues-module/MoreInfoDialogue.ts
@@ -49,7 +49,7 @@ export class MoreInfoDialogue extends Dialogue<
     );
     this.$content.append(this.$title);
 
-    this.$metadata = $('<div class="iiif-metadata-component"></div>');
+    this.$metadata = $('<article class="iiif-metadata-component"></article>');
     this.$content.append(this.$metadata);
 
     this.metadataComponent = new MetadataComponent({
@@ -98,5 +98,18 @@ export class MoreInfoDialogue extends Dialogue<
 
   resize(): void {
     this.setDockedPosition();
+
+    // always put tabindex on, so the metadata is focusable, 
+    // just in case there's something wrong with the height
+    // comparison below
+    this.$metadata.attr('tabindex', 0);
+    this.$metadata.attr('aria-label', this.config.content.title);
+
+    // if metadata's first group's height is lte than metadata (200px fixed I think), 
+    // there's no scroll happening, so no focus needed, and no aria label either
+    if(this.$metadata.find('.groups').first().height() <= this.$metadata.height()) {
+      this.$metadata.removeAttr('tabindex');
+      this.$metadata.removeAttr('aria-label');
+    }
   }
 }
