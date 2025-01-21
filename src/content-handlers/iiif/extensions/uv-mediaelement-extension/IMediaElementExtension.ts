2626e3685b6d17fd7db6a13520365c798ba3890d
Edward Silverton <e.silverton@gmail.com>
2025-01-21 10:37:25 +0000

    use accompanying canvas for poster image

--- src/content-handlers/iiif/extensions/uv-mediaelement-extension/IMediaElementExtension.ts
+++ src/content-handlers/iiif/extensions/uv-mediaelement-extension/IMediaElementExtension.ts
@@ -2,6 +2,6 @@ import { IExtension } from "../../modules/uv-shared-module/IExtension";
 
 export interface IMediaElementExtension extends IExtension {
   getEmbedScript(embedTemplate: string, width: number, height: number): string;
-  getPosterImageUri(): string;
+  getPosterImageUri(): string | null;
   isVideo(): boolean;
 }
