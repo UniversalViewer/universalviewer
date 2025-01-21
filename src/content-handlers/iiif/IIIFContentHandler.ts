UniversalViewer/universalviewer

commit 133e3c142c39c089d97249c0e8da6ad07a498b94

added preferMediaElementExtension config option (#1278)
It's now possible to use config to override which extensions get loaded depending on your use case. In this case, Exhibit only needs the simple AV experience even when manifests have structure.

src/content-handlers/iiif/IIIFContentHandler.ts
@@ -358,21 +358,6 @@ export default class IIIFContentHandler extends BaseContentHandler<IIIFData>
         }
       }
 
-      // if using uv-av-extension and there is no structure, fall back to uv-mediaelement-extension
-      const hasRanges: boolean = helper.getRanges().length > 0;
-
-      if (extension!.type === Extension.AV && !hasRanges) {
-        extension = await that._getExtensionByType(
-          Extension.MEDIAELEMENT,
-          format
-        );
-      }
-
-      // if there still isn't a matching extension, use the default extension.
-      if (!extension) {
-        extension = await that._getExtensionByFormat(Extension.DEFAULT.name);
-      }
-
       if (!data.locales) {
         data.locales = [];
         data.locales.push(defaultLocale);
@@ -385,6 +370,26 @@ export default class IIIFContentHandler extends BaseContentHandler<IIIFData>
       );
 
       data.config = await that.configure(config);
+
+      // if using uv-av-extension and there is no structure,
+      // or the preferMediaElementExtension config is set
+      // fall back to uv-mediaelement-extension
+      const hasRanges: boolean = helper.getRanges().length > 0;
+
+      if (
+        extension!.type === Extension.AV &&
+        (!hasRanges || data.config.options.preferMediaElementExtension)
+      ) {
+        extension = await that._getExtensionByType(
+          Extension.MEDIAELEMENT,
+          format
+        );
+      }
+
+      // if there still isn't a matching extension, use the default extension.
+      if (!extension) {
+        extension = await that._getExtensionByFormat(Extension.DEFAULT.name);
+      }
 
       that._createExtension(extension, data, helper);
     } catch (e) {

src/content-handlers/iiif/BaseConfig.ts
@@ -67,6 +67,9 @@ export type Options = {
   /** Determines if access control is pessimistic */
   pessimisticAccessControl?: boolean;
 
+  /** Determines if the mediaelement extension should be preferred */
+  preferMediaElementExtension?: boolean;
+
   /** Determines if viewport is preserved */
   preserveViewport?: boolean;
 
@@ -226,7 +229,7 @@ export type SettingsDialogueContent = DialogueContent & {
   clickToZoomEnabled: string;
   pagingEnabled: string;
   reducedMotion: string;
-  truncateThumbnailLabels: string; 
+  truncateThumbnailLabels: string;
   preserveViewport: string;
   title: string;
   website: string;

commit 74f785eb490275c8b0d0007c84d79015db2ed2e7

Fix an extension configuration bug introduced by #1278.

src/content-handlers/iiif/IIIFContentHandler.ts
@@ -264,6 +264,21 @@ export default class IIIFContentHandler extends BaseContentHandler<IIIFData>
     this.disposed = true;
   }
 
+  private async _loadAndApplyConfigToExtension(data: IUVData<any>, extension: any): Promise<void> {
+    const that = this;
+    // import the config file
+    if (!data.locales) {
+      data.locales = [];
+      data.locales.push(defaultLocale);
+    }
+    let config = await (extension).loadConfig(
+      data.locales[0].name,
+      extension?.type.name
+    );
+
+    data.config = await that.configure(config);
+  }
+
   private async _reload(data: IUVData<any>): Promise<void> {
     this._pubsub.dispose(); // remove any existing event listeners
 
@@ -358,18 +373,7 @@ export default class IIIFContentHandler extends BaseContentHandler<IIIFData>
         }
       }
 
-      if (!data.locales) {
-        data.locales = [];
-        data.locales.push(defaultLocale);
-      }
-
-      // import the config file
-      let config = await (extension as any).loadConfig(
-        data.locales[0].name,
-        extension?.type.name
-      );
-
-      data.config = await that.configure(config);
+      await this._loadAndApplyConfigToExtension(data, extension);
 
       // if using uv-av-extension and there is no structure,
       // or the preferMediaElementExtension config is set
@@ -384,11 +388,13 @@ export default class IIIFContentHandler extends BaseContentHandler<IIIFData>
           Extension.MEDIAELEMENT,
           format
         );
+        await this._loadAndApplyConfigToExtension(data, extension);
       }
 
       // if there still isn't a matching extension, use the default extension.
       if (!extension) {
         extension = await that._getExtensionByFormat(Extension.DEFAULT.name);
+        await this._loadAndApplyConfigToExtension(data, extension);
       }
 
       that._createExtension(extension, data, helper);