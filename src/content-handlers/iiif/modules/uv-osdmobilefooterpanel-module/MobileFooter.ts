24cec75c712a0f46bdabdc3092ab9561559be4d4
jamesmisson <jcmisson@gmail.com>
2025-03-17 17:03:09 +0000

    whitespace to bump build

--- src/content-handlers/iiif/modules/uv-osdmobilefooterpanel-module/MobileFooter.ts
+++ src/content-handlers/iiif/modules/uv-osdmobilefooterpanel-module/MobileFooter.ts
@@ -68,6 +68,7 @@ export class FooterPanel extends BaseFooterPanel<
 
     this.$zoomOutButton.onPressed(() => {
       this.extensionHost.publish(OpenSeadragonExtensionEvents.ZOOM_OUT);
+      
     });
 
     this.$rotateButton.onPressed(() => {
