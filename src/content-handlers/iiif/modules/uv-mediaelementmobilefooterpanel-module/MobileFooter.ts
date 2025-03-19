460592f6a588b83ba6da5d01f16e601da2373ef1
jamesmisson <jcmisson@gmail.com>
2025-03-19 14:32:09 +0000

    remove zoom buttons from mediaelement mobile footer

--- src/content-handlers/iiif/modules/uv-mediaelementmobilefooterpanel-module/MobileFooter.ts
+++ src/content-handlers/iiif/modules/uv-mediaelementmobilefooterpanel-module/MobileFooter.ts
@@ -1,15 +1,9 @@
-const $ = require("jquery");
 import { Config } from "../../extensions/uv-pdf-extension/config/Config";
-import { PDFExtensionEvents } from "../../extensions/uv-pdf-extension/Events";
 import { FooterPanel as BaseFooterPanel } from "../uv-shared-module/FooterPanel";
 
 export class FooterPanel extends BaseFooterPanel<
   Config["modules"]["mobileFooterPanel"]
 > {
-  $fullScreenBtn: JQuery;
-  $zoomInButton: JQuery;
-  $zoomOutButton: JQuery;
-
   constructor($element: JQuery) {
     super($element);
   }
@@ -18,28 +12,6 @@ export class FooterPanel extends BaseFooterPanel<
     this.setConfig("mobileFooterPanel");
 
     super.create();
-
-    this.$zoomOutButton = $(`
-      <button class="btn imageBtn zoomOut" title="${this.content.zoomOut}">
-          <i class="uv-icon-zoom-out" aria-hidden="true"></i>${this.content.zoomOut}
-      </button>
-  `);
-    this.$mainOptions.prepend(this.$zoomOutButton);
-
-    this.$zoomInButton = $(`
-      <button class="btn imageBtn zoomIn" title="${this.content.zoomIn}">
-          <i class="uv-icon-zoom-in" aria-hidden="true"></i>${this.content.zoomIn}
-      </button>
-  `);
-    this.$mainOptions.prepend(this.$zoomInButton);
-
-    this.$zoomInButton.onPressed(() => {
-      this.extensionHost.publish(PDFExtensionEvents.ZOOM_IN);
-    });
-
-    this.$zoomOutButton.onPressed(() => {
-      this.extensionHost.publish(PDFExtensionEvents.ZOOM_OUT);
-    });
   }
 
   resize(): void {
