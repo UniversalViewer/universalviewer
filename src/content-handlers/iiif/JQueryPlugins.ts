aeb66b7ad2a650926d69d6735108611f6331ffc6
K8Sewell <kait@notch8.com>
2025-07-01 16:36:54 -0500

    Remove not needed code

--- src/content-handlers/iiif/JQueryPlugins.ts
+++ src/content-handlers/iiif/JQueryPlugins.ts
@@ -558,13 +558,11 @@ interface JQuery {
   css: any;
   append: any;
   text: any;
-  link: any;
   toggle: any;
   html: any;
   empty: any;
   one: any;
   remove: any;
-  render: any;
   height: any;
   contents: any;
   outerWidth: any;
