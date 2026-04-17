f2ef621470735392eadd6d7621cadbf9c9f34a2a
Saira-A <139405429+Saira-A@users.noreply.github.com>
2026-04-17 16:04:04 +0100

    m3u8 mpd

--- src/content-handlers/iiif/IIIFContentHandler.ts
+++ src/content-handlers/iiif/IIIFContentHandler.ts
@@ -142,7 +142,9 @@ export default class IIIFContentHandler
     this._extensionRegistry[MediaType.GLB] = Extension.MODELVIEWER;
     this._extensionRegistry[MediaType.GLTF] = Extension.MODELVIEWER;
     this._extensionRegistry[MediaType.JPG] = Extension.OSD;
+    this._extensionRegistry[MediaType.M3U8] = Extension.AV;
     this._extensionRegistry[MediaType.MP3] = Extension.AV;
+    this._extensionRegistry[MediaType.MPD] = Extension.AV;
     this._extensionRegistry[MediaType.MPEG] = Extension.AV;
     this._extensionRegistry[MediaType.MPEG_DASH] = Extension.AV;
     this._extensionRegistry[MediaType.OPF] = Extension.EBOOK;
