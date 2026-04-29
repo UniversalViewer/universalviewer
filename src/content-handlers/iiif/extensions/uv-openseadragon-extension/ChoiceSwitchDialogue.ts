03a0522f5803c906717b85a44ee88548eee1d716
jamesmisson <jcmisson@gmail.com>
2026-04-29 16:36:17 +0100

    remove duplicated constructor and methods from dialogue

--- src/content-handlers/iiif/extensions/uv-openseadragon-extension/ChoiceSwitchDialogue.ts
+++ src/content-handlers/iiif/extensions/uv-openseadragon-extension/ChoiceSwitchDialogue.ts
@@ -1,17 +1,2 @@
 import { ChoiceSwitchDialogue as BaseChoiceSwitchDialogue } from "../../modules/uv-dialogues-module/ChoiceSwitchDialogue";
-import { Shell } from "../../modules/uv-shared-module/Shell";
-
-export class ChoiceSwitchDialogue extends BaseChoiceSwitchDialogue {
-  constructor($element: JQuery, shell: Shell) {
-    super($element, shell);
-  }
-
-  create(): void {
-    this.setConfig("choiceSwitchDialogue");
-    super.create();
-  }
-
-  resize(): void {
-    super.resize();
-  }
-}
+export class ChoiceSwitchDialogue extends BaseChoiceSwitchDialogue {}
