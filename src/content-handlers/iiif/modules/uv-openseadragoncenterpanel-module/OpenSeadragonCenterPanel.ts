6c450c59276bb7572d2e0a04475ef5d3a1e0dd5f
Saira-A <139405429+Saira-A@users.noreply.github.com>
2026-06-18 16:21:42 +0100

    Modify rotation to prevent download errors (#1779)

--- src/content-handlers/iiif/modules/uv-openseadragoncenterpanel-module/OpenSeadragonCenterPanel.ts
+++ src/content-handlers/iiif/modules/uv-openseadragoncenterpanel-module/OpenSeadragonCenterPanel.ts
@@ -237,7 +237,9 @@ export class OpenSeadragonCenterPanel extends CenterPanel<
   }
 
   rotateRight(): void {
-    this.viewer.viewport.setRotation(this.viewer.viewport.getRotation() + 90);
+    const current = this.viewer.viewport.getRotation();
+    const next = (current + 90) % 360;
+    this.viewer.viewport.setRotation(next);
   }
 
   updateResponsiveView(): void {
